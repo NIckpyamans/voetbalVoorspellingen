c7438363e0a2b5371b364c4a66d3394aae5beb95
Nick Paymans <npaymans@romijnders.nl>
2026-03-18 13:42:30 +0100

    Update velocityEngine.ts

--- services/velocityEngine.ts
+++ services/velocityEngine.ts
@@ -1,5 +1,3 @@
-// services/velocityEngine.ts
-import { Match } from "../types";
 import { fetchMatchesAndPredictions, MatchesUpdate } from "./matchService";
 
 class VelocityEngine {
@@ -10,36 +8,49 @@ class VelocityEngine {
 
   subscribe(cb: (data: MatchesUpdate) => void) {
     this.subscribers.push(cb);
-    return () => { this.subscribers = this.subscribers.filter(s => s !== cb); };
+    return () => {
+      this.subscribers = this.subscribers.filter((s) => s !== cb);
+    };
   }
 
   async startPulse(date: string) {
-    if (this.interval) { clearInterval(this.interval); this.interval = null; }
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+
     this.currentDate = date;
     await this.fetch(date);
 
-    const isToday = date === new Date().toISOString().split('T')[0];
-    const ms = isToday ? 90_000 : 300_000;
+    const isToday = date === new Date().toISOString().split("T")[0];
+    const ms = isToday ? 15_000 : 300_000;
+
     this.interval = window.setInterval(async () => {
-      if (this.currentDate === date) await this.fetch(date);
+      if (this.currentDate === date) {
+        await this.fetch(date);
+      }
     }, ms);
   }
 
   private async fetch(date: string) {
     if (this.running) return;
     this.running = true;
+
     try {
       const data = await fetchMatchesAndPredictions(date);
-      this.subscribers.forEach(s => s(data));
-    } catch (e) {
-      console.error('[VelocityEngine]', e);
+      this.subscribers.forEach((s) => s(data));
+    } catch (err) {
+      console.error("[VelocityEngine]", err);
     } finally {
       this.running = false;
     }
   }
 
   stopPulse() {
-    if (this.interval) { clearInterval(this.interval); this.interval = null; }
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
     this.currentDate = null;
   }
 }
